2dfcd5e15fdd63a58a21ecd3d829c095f29c5e0d
Cedric <fyccedric@gmail.com>
2025-02-21 10:49:39 +0100

    Repo re-initialised
    gitignore updated and repo name updated

---
+++ Basic Models 2/DP_Motor_with_Mass_on_Pulley_Script.m
@@ -0,0 +1,16 @@
+simtime = 31;
+d_p_pump = 1;
+V_theo = 2E-3;
+eta_hm = 0.95;
+eta_vol = 0.95;
+M_L = 1.2E-2;
+J_Mo = 5.5E-3;
+k_R = 12;
+r_D = 0.03;
+c = 0.8;
+d_R = 1.2;
+g = 9.81;
+m = 0.6;
+R_pipe = 5;
+
+sim('DP_Motor_with_Mass_on_Pulley')
